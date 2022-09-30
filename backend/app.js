a5e2fa127271057661ab57426920369521f79f32
siskiuslide <jakewhatling2002@gmail.com>
2022-09-30 13:43:34 +0100

    Linked get request to backend

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser"); //now seperate to express
+const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const dotenv = require("dotenv").config;
 dotenv({ path: "./config.env" });
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json({ limit: "10kb" })); //allows json to be parsed in req.body
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(morgan("dev"));
 const DB = process.env.DATABASE;
 
@@ -16,6 +17,7 @@ mongoose.connect(DB).then((con) => {
   console.log("connected to mongodb");
 });
 const garageRoute = require("./routes/garage");
+
 app.use("/garage", garageRoute);
 
 module.exports = app;

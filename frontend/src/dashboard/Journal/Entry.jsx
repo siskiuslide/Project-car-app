10c1ba94de5221b3481573a0a2b6d85ec5dda08b
siskiuslide <jakewhatling2002@gmail.com>
2022-11-01 10:11:56 +0000

    Formatted timestamp into date on todo dashboard

--- frontend/src/dashboard/Journal/Entry.jsx
+++ frontend/src/dashboard/Journal/Entry.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./JournalEntries.css";
 import "../../App.css";
+import { useEffect, useState } from "react";
 
 const Entry = (props) => {
+  const [date, setDate] = useState(new Date(props.date).getDate());
+  const [month, setMonth] = useState(new Date(props.date).getMonth());
+  const [year, setYear] = useState(new Date(props.date).getFullYear());
+
   return (
     <div className="Entry">
-      <p style={{ width: "10%" }}>{props.date}</p>
-      <p style={{ width: "15%" }}>{props.category}</p>
-      <p style={{ width: "30%" }}>{props.description}</p>
-      <p style={{ width: "20%" }} className="material-icons">
+      <p style={{ width: "15%" }}>{`${date}/${month}/${year}`}</p>
+      <p style={{ width: "15%", textTransform: "capitalize" }}>{props.category}</p>
+      <p style={{ width: "35%" }}>{props.description}</p>
+      <p style={{ width: "20%", textAlign: "center" }} className="material-icons">
         {props.completed ? "check_box" : "check_box_outline_blank"}
       </p>
       <div className="options" style={{ display: "flex", width: "10%" }}>
